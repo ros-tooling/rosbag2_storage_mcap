290d3b813bbcd7bc357afaf5d8be094806618783
John Hurliman <jhurliman@jhurliman.org>
2022-03-18 12:00:55 -0700

    1.0.0
    Signed-off-by: John Hurliman <jhurliman@jhurliman.org>

--- rosbag2_storage_mcap/src/message_definition_cache.cpp
+++ rosbag2_storage_mcap/src/message_definition_cache.cpp
@@ -1,4 +1,4 @@
-// Copyright 2022, Amazon.com Inc or its Affiliates. All rights reserved.
+// Copyright 2022, Foxglove Technologies. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
